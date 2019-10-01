9a674aa2a0840d9ea08e299b768a3de57fc235ce
Armando Cardona <cardonaarmando95@gmail.com>
2019-10-01 14:15:18 -0500

    added routing for add friends

--- eventr/src/router.js
+++ eventr/src/router.js
@@ -18,6 +18,22 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+    },
+    {
+      path: '/eventCreator',
+      name: 'eventCreator',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "about" */ './views/eventCreator.vue')
+    },
+    {
+      path: '/friendsHub',
+      name: 'friendsHub',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "about" */ './views/friendsHub.vue')
     }
   ]
 })
