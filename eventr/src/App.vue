33519348a36a6b6d7fe6e96275ab246b6c15bb71
Armando Cardona <cardonaarmando95@gmail.com>
2019-10-02 14:20:05 -0500

    Chnaged the home website
    Changed the home website and added routing for other pages

--- eventr/src/App.vue
+++ eventr/src/App.vue
@@ -1,23 +1,52 @@
 <template>
   <v-app>
-    <v-app-bar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>EVENT</span>
-        <span>R</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        text
-        href="https://github.com/MichaelClayMoore/Team_404"
-        target="_blank"
+    <v-app-bar
+      :clipped-left="$vuetify.breakpoint.lgAndUp"
+      app
+      color="red darken-3"
+      dark
+    >
+      <v-toolbar-title
+        style="width: 300px"
+        class="ml-0 pl-4"
       >
-        <span class="mr-2">Github Link</span>
+        <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+        <span class="hidden-sm-and-down">Eventr | Friends</span>
+      </v-toolbar-title>
+      <v-text-field
+        flat
+        solo-inverted
+        hide-details
+        prepend-inner-icon="search"
+        label="Search"
+        class="hidden-sm-and-down"
+      ></v-text-field>
+      <div class="flex-grow-1"></div>
+      <v-btn icon>
+        <v-icon>mdi-apps</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>mdi-bell</v-icon>
+      </v-btn>
+      <v-btn
+        icon
+        large
+      >
+        <v-avatar
+          size="32px"
+          item
+        >
+          <v-img
+            src=".\assets\headshot_002.jpg"
+            alt="Vuetify"
+          >
+          </v-img></v-avatar>
       </v-btn>
     </v-app-bar>
-    
     <v-content>
       <div id="nav">
-        <router-link to="/about">About</router-link>
+        <router-link to="/home">Home</router-link>
+        <router-link to="/about"> | About</router-link>
         <router-link to="/eventCreator"> | Event Creator</router-link>
         <router-link to="/friendsHub"> | Add Friend</router-link>
       </div>
--- eventr/src/views/friendsHub.vue
+++ eventr/src/views/friendsHub.vue
@@ -76,49 +76,7 @@
       </v-list>
     </v-navigation-drawer>
 
-    <v-app-bar
-      :clipped-left="$vuetify.breakpoint.lgAndUp"
-      app
-      color="blue darken-3"
-      dark
-    >
-      <v-toolbar-title
-        style="width: 300px"
-        class="ml-0 pl-4"
-      >
-        <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
-        <span class="hidden-sm-and-down">Eventr Friends</span>
-      </v-toolbar-title>
-      <v-text-field
-        flat
-        solo-inverted
-        hide-details
-        prepend-inner-icon="search"
-        label="Search"
-        class="hidden-sm-and-down"
-      ></v-text-field>
-      <div class="flex-grow-1"></div>
-      <v-btn icon>
-        <v-icon>mdi-apps</v-icon>
-      </v-btn>
-      <v-btn icon>
-        <v-icon>mdi-bell</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        large
-      >
-        <v-avatar
-          size="32px"
-          item
-        >
-          <v-img
-            src="..\assets\headshot_002.jpg"
-            alt="Vuetify"
-          >
-          </v-img></v-avatar>
-      </v-btn>
-    </v-app-bar>
+        
     <v-content>
       <v-container
         class="fill-height"
