11bdf7d94a2a2e511070ca16d015a6b93d465295
mcm0399 <mcm0399@my.unt.edu>
2019-10-11 15:53:41 -0500

    worked on page a bit.

--- eventr/src/router.js
+++ eventr/src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import EventCreator from './views/eventCreator.vue'
 
 Vue.use(Router)
 
@@ -25,7 +26,7 @@ export default new Router({
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/eventCreator.vue')
+      component: EventCreator
     }
   ]
 })
