ae5792c0ce02e567c671a3b4a50481eba6ea052b
mcm0399 <mcm0399@my.unt.edu>
2019-10-18 17:19:48 -0500

    commited

--- eventr/src/views/eventCreator.vue
+++ eventr/src/views/eventCreator.vue
@@ -1,79 +1,156 @@
 <template>
   <v-container>
-    <v-dialog
-      v-model="dialog"
-      max-width="50%"
-    >
+
+    <!--
+    ______ date dialog _________________________________________________________________
+    | this is the date dialog. this will appear when the date button.                  |
+    | v-model = dateDialog                                                             |
+    |     -> this is what allows this to be visible. if yes, it will be visible.       |
+    |        otherwise it is hidden.                                                   |
+    L__________________________________________________________________________________|
+    -->
+    <v-dialog v-model="dateDialog" max-width="50%">
       <v-card>
-        <v-card-title class="tilte">When is it?</v-card-title>
+        <v-card-title :style="{'background-color':'tomato','color':'white'}" class="title">When is it?</v-card-title>
         <v-card-actions>
           <v-date-picker landscape full-width color="#ff6347" v-model="eventDate" :style="{'font-size':'15px'}"></v-date-picker>
         </v-card-actions>
       </v-card>
     </v-dialog>
-    <v-flex flex justify-center align-center column>
-    <h1 :style="{'display':'inline','font-weight':'bold','font-size':'48px'}"><span class="heading">Create a Event </span></h1>
-    <i class="material-icons" :style="{'font-size':'36px','position':'relative','top':'5px','color':'tomato'}">whatshot</i>
-    <v-layout column>
-      <!-- <Span class="heading"> The essentials.</Span>
-      <v-divider/>
-      <br/> -->
-      <v-text-field class="input" id="name" label="Event Name" color="#ff6347"></v-text-field>
-      <v-btn color="#ff6347" :style="{'color':'#ffffff'}" @click="dialog = true;">{{test}}</v-btn>
-      <div class="d-flex flex-row align-content-space-between" >
-        <v-spacer/>
-        <v-checkbox class="flex-grow-1 flex-shrink-0" label="RSVP Required" color="#ff6347"></v-checkbox>
-        <v-checkbox class="flex-grow-1 flex-shrink-0" label="Condition 2" color="#ff6347"></v-checkbox>
-        <v-checkbox class="flex-grow-1 flex-shrink-0" label="Condition 3" color="#ff6347"></v-checkbox>
-        <v-checkbox class="flex-grow-1 flex-shrink-0" label="Condition 4" color="#ff6347"></v-checkbox>
-      </div>
-      <Span class="heading"> Tell us a little about it.</Span>
-      <v-textarea filled clearable></v-textarea>
-      <v-select v-model="eventName" :items="Events" filled rounded class="input" id="test" label="Event Type" color="#ff6347"></v-select>
 
-      <v-container>
+    <!--
+    ______ location dialog _____________________________________________________________
+    | this is the location dialog. this will appear when the location button.          |
+    | v-model = locationDialog                                                         |
+    |     -> this is what allows this to be visible. if yes, it will be visible.       |
+    |        otherwise it is hidden.                                                   |
+    L__________________________________________________________________________________|
+    -->
+    <v-dialog v-model="locationDialog" max-width="50%">
+      <v-card>
+        <!-- title bar -->
+        <v-card-title :style="{'background-color':'tomato','color':'white'}" class="title">Where is it?</v-card-title>
+
+        <!-- input for the address -->
+        <v-card-text>
+          <v-layout row>
+          <v-text-field class="input" label="Address 1"></v-text-field>
+          <v-text-field class="input" label="Address 2 (optional)"></v-text-field>
+          <v-text-field class="input" label="City"></v-text-field>
+          <v-text-field class="input" label="State"></v-text-field>
+          <v-text-field class="input" label="Zip"></v-text-field>
+          </v-layout>
+        </v-card-text>
+
+        <!-- this is the or... on the card -->
+        <v-card-text>
+          <div class="d-flex flex-row align-content-space-between" >
+            <v-spacer/>
+            <span class="title">or...</span>
+            <v-spacer/>
+          </div>
+        </v-card-text>
+
+        <!-- this is the container for the map. it is currently under dev and not fully finished -->
+        <v-card-text>
+          <div>
+            <p>you can also use this map...</p>
+            <div style="background-image: linear-gradient(to bottom right, green, #f06d06); height:200px"></div>
+          </div>
+        </v-card-text>
+
+      </v-card>
+    </v-dialog>
+
+    <!-- this is where the full page starts -->
+    <v-flex flex justify-center align-center column>
+      <!-- title line -->
+      <h1 :style="{'display':'inline','font-weight':'bold','font-size':'48px'}"><span class="heading">Create a Event </span></h1>
+      <i class="material-icons" :style="{'font-size':'36px','position':'relative','top':'5px','color':'tomato'}">whatshot</i>
+
+      <!-- content section   -->
+      <v-layout column>
+
+        <!-- event name field -->
+        <v-text-field v-model="eventName" class="input" id="name" label="Event Name" color="#ff6347"></v-text-field>
+
+        <!-- both the location and date buttons. -->
         <v-layout row>
-          <v-text-field label="What do you need?"></v-text-field>
           <v-spacer/>
-          <v-text-field label="What do you want?"></v-text-field>
+          <v-btn color="#ff6347" :style="{'color':'#ffffff'}" @click="dateDialog = true;">{{eventDateString}}</v-btn>
+          <v-spacer/>
+          <v-btn color="#ff6347" :style="{'color':'#ffffff'}" @click="locationDialog = true;">{{eventLocation}}</v-btn>
+          <v-spacer/>
         </v-layout>
-      </v-container>
-      <v-text-field label="Invite people" hint="Enter the username of your friend or someone you know."></v-text-field>
-    </v-layout>
+
+        <!-- rsvp checkbox -->
+        <div class="d-flex flex-row align-content-space-between" >
+          <v-spacer/>
+          <v-checkbox class="flex-grow-1 flex-shrink-0" label="RSVP Required" color="#ff6347"></v-checkbox>
+        </div>
+
+        <!-- tell us a little about section -->
+        <Span class="heading"> Tell us a little about it.</Span>
+        <v-textarea filled clearable></v-textarea>
+
+        <!--
+        ______ Event style selector ________________________________________________________
+        | this is the style selector. the user will only be able to select one of the      |
+        | provided items in the Events variable.                                           |
+        | v-model = eventStyle                                                             |
+        |     -> this is what hold the value of what they select. originally set to the    |
+        |        empty string. it will be replaced with whatever value they select.        |
+        L__________________________________________________________________________________|
+        -->
+        <v-select v-model="eventStyle" :items="Events" filled rounded class="input" label="Event Type" color="#ff6347"></v-select>
+
+        <!-- this hold the 'where is it' input -->
+        <v-container>
+          <v-layout row>
+            <v-text-field label="Where is it?"></v-text-field>
+          </v-layout>
+        </v-container>
+
+        <!--
+         ______ invite people field ________________________________________________________
+        | this is where they will be able to invite friends.                               |
+        | this is something to be implemented when friends are added to our site           |
+        | as well as our database.                                                         |
+        L__________________________________________________________________________________|
+        -->
+        <v-text-field label="Invite people" hint="Enter the username of your friend or someone you know."></v-text-field>
+      </v-layout>
   </v-flex>
-  <!-- v-layout row>
-     <v-text-field label="What do you need?"></v-text-field>
-    <v-spacer/>
-    <v-text-field label="What do you want?"></v-text-field> 
-    <v-spacer/>
-    <v-btn color="#ff6347" :style="{'color':'#ffffff'}" @click="">
-      <i class="material-icons">
-        arrow_back_ios
-      </i>
-      Back
-    </v-btn>
-    <v-spacer/>
-    <v-btn color="#ff6347" :style="{'color':'#ffffff'}" @click="">Next
-      <i class="material-icons">
-        arrow_forward_ios
-      </i>
-    </v-btn>
-    <v-spacer/>
-  </v-layout -->
 </v-container>
 </template>
 
+<!--
+______ script portion ______________________________________________________________
+| this is the portion of the file that has to deal with all the javascript.        |
+L__________________________________________________________________________________|
+-->
 <script>
 export default {
+  // name of the file/component
   name: 'eventCreator',
+
+  // all the initial data for the component.
   data () {
     return {
+      // data that will be changed by the user
       eventName: "",
-      dataDia: false,
-      test: "choose a date",
-      dialog: false,
-      eventStyle: "",
+      eventLocation: "choose a location",
       eventDate: new Date().toISOString().substr(0,10),
+      eventStyle: "",
+
+      // used for displaying the date to the user
+      eventDateString: "choose a date",
+
+      // data used to control page flow
+      locationDialog: false,
+      dateDialog: false,
+
+      // data used for the user to select from
       Events: [
         "Birthday",
         "Get Together",
@@ -82,68 +159,70 @@ export default {
       ]
     }
   },
+
+  // this section is to watch variables. Anytime a variable with a corresponding
+  // name changes value, the function will run. the val that is passed into
+  // every function is the new value that will be set.
   watch :{
+
+    // if the eventDate variable changes, they are trying to select a date
     eventDate(val){
-      this.dialog = false;
+      // hide the dialog because they have choosen now.
+      this.dateDialog = false;
+
+      // gets the new date that was chosen
       let tempDate = new Date(val);
-      this.test = tempDate.toDateString();
+      tempDate.setDate( tempDate.getDate() + 1 )
+
+      // sets the display variable so the user can see what they chose
+      this.eventDateString = tempDate.toDateString();
     }
   },
+
+  // this is the methods portion. This is used to hold functions that our
+  // page will use.
   methods :{
-    showDatePicker(){
-      console.log("test");
-      this.dialog = true;
-    }
+    // currently none
   }
 }
 </script>
 
+<!--
+______ style _portion ______________________________________________________________
+| this is the portion of the file that has to deal with all the defined css styles.|
+| the scoped prop says that any style defined here is only defined here.           |
+L__________________________________________________________________________________|
+-->
 <style scoped>
-  .about{
-    /* background: linear-gradient(to top right,#ffc000, #ff5004); */
-    /* background: linear-gradient(to top right, #F2865E, #F2C641, #05C7F2, #049DD9, #F2167D); */
-    /* background: linear-gradient(to top right, #FEEAF2, #FFCB1F, #12BAC2, #0BDBE6, #F01677, #ff1100); */
-    /* height: 80vh; */
-  }
 
-  .v-text-field{
-    display: block;
-  }
-
+  /* style for elements that have the title clas */
   .title{
-    /* 'text-align':'center', 'font-family':'\"Times New Roman\"\, Times\, serif' */
+    /* sets text to center and a bold 24px Montserrat font */
     text-align: center;
     font-family: 'Montserrat', sans-serif;
+    font-size: 24px;
     font-weight: bold;
+
+    /* removes bottom margin */
     margin-bottom: 0px;
-    font-size: 24px;
   }
 
-  .timeLine{
-    align-self: center;
+  /* style for imput class */
+  .input{
+    /* adds 5px to left and right of element */
+    margin-left: 5px;
+    margin-right: 5px;
   }
 
+  /* style for heading class */
   .heading{
+    /* sets text to center and a 24px Montserrat font*/
     text-align: center;
     font-family: 'Montserrat', sans-serif;
-    margin-bottom: 0px;
     font-size: 24px;
-  }
 
-  #test{
-    margin-top: 0px;
-    width: 50vw;
-    display: inline;
-  }
-
-  #style{
-    width: 250px;
-    height: 40px;
-    background-color: #F44336;
-    color: #ffffff;
-    align-items: center;
-    margin: 5px;
-    display: block;
+    /* removes margins from the bottom */
+    margin-bottom: 0px;
   }
 
 </style>
