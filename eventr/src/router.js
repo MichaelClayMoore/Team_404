577bc487666d51ccb1c57473f94840e409eb1545
Benson4 <benson.phillipsiv@gmail.com>
2019-10-30 18:52:10 -0500

    Created page
    Having issues creating a date range selector

--- eventr/src/router.js
+++ eventr/src/router.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
 import EventCreator from './views/eventCreator.vue'
+import SearchEvent from './views/searchEvent.vue'
+import Test from './views/test.vue'
 
 Vue.use(Router)
 
@@ -27,6 +29,16 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: EventCreator
+    },
+    {
+      path: '/searchEvent',
+      name: 'searchEvent',
+      component: SearchEvent
+    },
+    {
+    path: '/test',
+      name: 'test',
+      component: Test
     }
   ]
 })
