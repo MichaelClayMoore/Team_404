2ab0ff86aba4d36bccbb7354e897e8e3a9b9d022
Armando Cardona <cardonaarmando95@gmail.com>
2019-11-03 13:14:07 -0600

    changed header

--- eventr/src/App.vue
+++ eventr/src/App.vue
@@ -1,36 +1,10 @@
 <template>
   <v-app>
-    <v-app-bar
-      :clipped-left="$vuetify.breakpoint.lgAndUp"
-      app
-      color="red darken-3"
-      dark
-    >
-      <v-toolbar-title style="width: 300px" class="ml-0 pl-4" >
-        <span class="hidden-sm-and-down">Eventr</span>
+    <v-app-bar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>Eventr</span>
       </v-toolbar-title>
-      
-      <div class="flex-grow-1"></div>
-      <v-btn icon>
-        <v-icon>mdi-apps</v-icon>
-      </v-btn>
-      <v-btn icon>
-        <v-icon>mdi-bell</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        large
-      >
-        <v-avatar
-          size="32px"
-          item
-        >
-          <v-img
-            src=".\assets\headshot_002.jpg"
-            alt="Vuetify"
-          >
-          </v-img></v-avatar>
-      </v-btn>
+      <v-spacer></v-spacer>
     </v-app-bar>
     <v-content>
       <div id="nav" class="ml-0 pl-4">
