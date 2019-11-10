e5b183fd40c3b94292a14bff87326626ee005c54
mcm0399 <mcm0399@my.unt.edu>
2019-11-10 17:48:24 -0600

    added geocoding for events

--- eventr/Backend/events/eventController.py
+++ eventr/Backend/events/eventController.py
@@ -1,14 +1,19 @@
 import json
+from Utility.geocodeUtility import geocodeUtility
 
 class eventController:
     def __init__(self):
         self.events = []
+        self.geoCoder = geocodeUtility();
 
     def test(self):
         return "test"
 
     def save_event(self, event ):
         try:
+            latlong = self.geocode_location(event['location'])
+            event['location']['latitude'] = latlong['latitude']
+            event['location']['longitude'] = latlong['longitude']
             self.events.append(event);
             return "Success"
         except:
@@ -16,3 +21,8 @@ class eventController:
 
     def return_events(self):
         return json.dumps( self.events )
+
+
+    def geocode_location(self, location):
+        latlong = self.geoCoder.getLatLong(location)
+        return latlong
