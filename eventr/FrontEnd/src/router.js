ff213cdf48e1db5e1af08937889d067527429c46
mcm0399 <mcm0399@my.unt.edu>
2019-11-10 14:54:40 -0600

    made changes to event creator

--- eventr/FrontEnd/src/router.js
+++ eventr/FrontEnd/src/router.js
@@ -20,7 +20,7 @@ export default new Router({
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
     },
     {
-      path: '/eventCreator',
+      path: '/event_creator',
       name: 'eventCreator',
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
