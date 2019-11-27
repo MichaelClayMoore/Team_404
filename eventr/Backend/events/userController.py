0901856bd3e40087106835539fe33e60c9ee7ed3
thelastdanicorn <55297287+thelastdanicorn@users.noreply.github.com>
2019-11-26 18:23:19 -0600

    changed what is returned

--- eventr/Backend/events/userController.py
+++ eventr/Backend/events/userController.py
@@ -13,7 +13,7 @@ class userController:
             addUserToDatabase = self.userDAO.addUser(user);
             if addUserToDatabase:
                 user['id'] = addUserToDatabase
-            return json.dumps(user)
+            return json.dumps( user )
             return json.dumps(False)
         except:
             return json.dumps(False)
